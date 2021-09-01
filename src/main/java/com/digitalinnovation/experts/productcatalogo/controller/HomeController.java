a7fe43343f83f3ebc3dd6dba1351bdcf385a72af
alinealien <alinebdantunes@gmail.com>
2021-08-31 21:48:41 -0300

    Segunda atualização

---
+++ src/main/java/com/digitalinnovation/experts/productcatalogo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.experts.productcatalogo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World! Eu sou Aline Antunes";
+    }
+}
---
+++ src/main/java/com/digitalinnovation/experts/productcatalogo/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.digitalinnovation.experts.productcatalogo.controller;
+
+public class ProductController {
+}
